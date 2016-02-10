2c971e97da6ec4fe2f63feaa2328767a0176e3dc
tavinb <tavinb@gmail.com>
2016-02-10 12:21:49 -0800

    home page and about page with new image on refresh

--- js/isotope.js
+++ js/isotope.js
@@ -1,6 +1,6 @@
 // external js: isotope.pkgd.js
 
-$( document ).ready( function() {
+$(document).ready(function () {
   // init Isotope
   var $container = $('.isotope').isotope({
     itemSelector: '.grid-item',
@@ -10,9 +10,9 @@ $( document ).ready( function() {
       //symbol: '.symbol',
       //number: '.number parseInt',
       category: '[data-category]',
-      weight: function( itemElem ) {
-        var weight = $( itemElem ).find('.weight').text();
-        return parseFloat( weight.replace( /[\(\)]/g, '') );
+      weight: function (itemElem) {
+        var weight = $(itemElem).find('.weight').text();
+        return parseFloat(weight.replace(/[\(\)]/g, ''));
       }
     }
   });
@@ -20,22 +20,22 @@ $( document ).ready( function() {
   // filter functions
   var filterFns = {
     // show if number is greater than 50
-    numberGreaterThan50: function() {
+    numberGreaterThan50: function () {
       var number = $(this).find('.number').text();
-      return parseInt( number, 10 ) > 50;
+      return parseInt(number, 10) > 50;
     },
     // show if name ends with -ium
-    ium: function() {
+    ium: function () {
       var name = $(this).find('.name').text();
-      return name.match( /ium$/ );
+      return name.match(/ium$/);
     }
-  }; 
+  };
 
   // bind filter button click
-  $('#filters').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
+  $('#filters').on('click', 'button', function () {
+    var filterValue = $(this).attr('data-filter');
     // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
+    filterValue = filterFns[filterValue] || filterValue;
     $container.isotope({ filter: filterValue });
   });
 
@@ -48,11 +48,11 @@ $( document ).ready( function() {
   */
   
   // change is-checked class on buttons
-  $('.button-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
+  $('.button-group').each(function (i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'button', function () {
       $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $( this ).addClass('is-checked');
+      $(this).addClass('is-checked');
     });
   });
   
